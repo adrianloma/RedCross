ffdbd90b2e8e52f0d789f0ae779cabd40e8f25b6
Andrés Cavazos <andrscvz@gmail.com>
2016-04-30 18:56:46 -0500

    Boton regresar no funcionaba en pantalla de calificaciones y asistencias

--- vistas/seleccion/seleccionAlumnoCalifAsist.php
+++ vistas/seleccion/seleccionAlumnoCalifAsist.php
@@ -106,7 +106,7 @@
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="../menus/menuMaestro.php">Regresar</a></li>
+					<li><a href="seleccionCursoCalifAsist.php">Regresar</a></li>
 				</ul>
 			</div>
 		</div>
